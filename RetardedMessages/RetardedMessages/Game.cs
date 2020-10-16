b61a4acc72aff78402db9401c651e97b0eb850bd
Polininium <69752526+Polininium@users.noreply.github.com>
2020-10-16 19:52:45 +0300

    Add files via upload

---
+++ RetardedMessages/RetardedMessages/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetardedMessages
+{
+    class Game
+    {
+        
+
+        static void TheGame()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(100);
+            Console.WriteLine(num);
+        } 
+
+    }
+}
